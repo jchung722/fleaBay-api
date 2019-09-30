857bb800929cc59bf28d95fe3b234d0dfd50b5da
Jessica Chung <jchung722@gmail.com>
2019-09-30 17:16:32 -0500

    update routes and add home controller for simple json message

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+    render json: {greeting: 'Welcome to fleaBay!'}
+  end
+end
---
+++ test/controllers/home_controller_test.rb
@@ -0,0 +1,7 @@
+# require "test_helper"
+
+# describe HomeController do
+#   it "does a thing" do
+#     assert_equals 2, 2
+#   end
+# end

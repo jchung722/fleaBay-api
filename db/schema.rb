f80785d319c3c498b3ea88b36a77165506308566
Jessica Chung <jchung722@gmail.com>
2019-10-02 11:16:30 -0500

    add lost migration file

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_10_01_205637) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', null: false
-    t.string 'password_digest', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
+
 end

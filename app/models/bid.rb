3c0a8b31d6b0bed60f9d6d09aab7203751c5f31b
Jessica Chung <jchung722@gmail.com>
2019-10-07 16:54:08 -0700

    add custom validation methods to bid for bid amount and auction date

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -2,4 +2,14 @@ class Bid < ApplicationRecord
   belongs_to :user
   belongs_to :auction
   validates :amount, presence: true
+  validate :amount_must_be_highest_bid
+  validate :auction_cannot_be_over
+
+  def amount_must_be_highest_bid
+    errors.add(:amount, "can't be lower than highest bid") unless auction && amount > auction.highest_bid_amount
+  end
+
+  def auction_cannot_be_over
+    errors.add(:auction, "can't be over") unless auction && auction.end_date > Date.today
+  end
 end

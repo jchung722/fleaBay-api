5ec15a572150200559f2ad1bbd55c400181b4f71
Jessica Chung <jchung722@gmail.com>
2019-10-07 22:38:01 -0700

    delete unnecessary home controller and update routes

--- app/controllers/home_controller.rb
+++
@@ -1,5 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    render json: { greeting: 'Welcome to fleaBay!' }
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
   resources :auctions
+
   resource :bids, only: [:create], path: '/' do
     get :create, on: :member, path: '/auctions/:id/bids'
   end
+
   post '/auctions/:id/bids/', to: 'bids#create'
+
   post '/refresh', to: 'refresh#create'
+
   post '/signup', to: 'signup#create'
+
   post '/signin', to: 'signin#create'
+
   delete '/signin', to: 'signin#destroy'
 end
